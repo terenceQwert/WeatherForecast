be0e23aa96d1e76215b71d8ca71349e68afb2b90
s8854086 <s8854086@gmail.com>
2021-05-05 08:32:32 +0800

    Add project files.

---
+++ RestFulApi/Controllers/ValuesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestFulApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        static public string[] valueS = new string[]{"update", "test2"};
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return valueS;
+//            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            valueS = new string[] { value, value };
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+            valueS = new string[] { "id4", "put data to update" };
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
